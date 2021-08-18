26e0dd5788e8973b3e9900b96f4be0a1c29f4748
Yago <yagocarvalho.dev@gmail.com>
2021-08-18 16:31:31 -0300

    Resolução exercício 3

--- m6_exercicios/exercicio3.js
+++ m6_exercicios/exercicio3.js
@@ -1,11 +1,17 @@
 /*
-    Desenvolva uma função que recebe dois parâmetros, um é a quantidade de horas trabalhadas
-    por um funcionário num mês, e o quanto ele recebe por hora. O retorno da função deve ser 
-    a string "Salário igual a R$ X", em que X é o quanto o funcionário ganhou no mês.
+    Desenvolva uma função que recebe dois parâmetros, um é a quantidade de horas trabalhadas 
+    por um funcionário num mês, e o quanto ele recebe por hora. A função deverá calcular o 
+    salário líquido mensal do funcionário, que é da quantidade de horas trabalhadas no mês 
+    multiplicada pelo valor da sua hora. Desse valor, deve ser subtraído 30%, relativo a 
+    impostos. O retorno da função deve ser a string "Salário igual a R$ X", em que X é o 
+    quanto o funcionário ganhou no mês.
 */
 
 function calcularSalario(horasTrabalhadas, valorHora) {
-  return `Salário igual a R$ ${horasTrabalhadas * valorHora}`;
+  const salarioBruto = horasTrabalhadas * valorHora;
+  const salarioLiquido = salarioBruto - salarioBruto * 0.3;
+  return `Salário igual a R$ ${salarioLiquido}`;
 }
 
-console.log(calcularSalario(150, 40.5));
+console.log(calcularSalario(150, 40.5)); // Salário igual a R$ 4252.5
+console.log(calcularSalario(10, 100)); // Salário igual a R$ 700
