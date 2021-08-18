ff88c54b306bf94b58d493cacc98e9c987d0f4e1
Yago <yagocarvalho.dev@gmail.com>
2021-08-18 15:14:27 -0300

    Resolução do exercício 3

--- m6_exercicios/exercicios3.js
+++ m6_exercicios/exercicios3.js
@@ -1,5 +1,11 @@
 /*
-    Desenvolva uma função que recebe dois parâmetros, um é a quantidade de horas trabalhadas por
-    um funcionário
+    Desenvolva uma função que recebe dois parâmetros, um é a quantidade de horas trabalhadas
+    por um funcionário num mês, e o quanto ele recebe por hora. O retorno da função deve ser 
+    a string "Salário igual a R$ X", em que X é o quanto o funcionário ganhou no mês.
 */
 
+function calcularSalario(horasTrabalhadas, valorHora) {
+  return `Salário igual a R$ ${horasTrabalhadas * valorHora}`;
+}
+
+console.log(calcularSalario(150, 40.5));
