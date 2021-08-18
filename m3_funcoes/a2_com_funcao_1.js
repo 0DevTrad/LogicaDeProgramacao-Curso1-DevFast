2ad44a24d44d2674beb9599d039a6c7aa6683b7e
Yago <yagocarvalho.dev@gmail.com>
2021-08-18 19:23:32 -0300

    teste

--- m3_funcoes/a2_com_funcao_1.js
+++ m3_funcoes/a2_com_funcao_1.js
@@ -3,7 +3,7 @@ const m1 = 7;
 const a1 = 2021;
 
 function exibirData1() {
-    console.log(`${d1}/${m1}/${a1}`);
+  console.log(`${d1}/${m1}/${a1}`);
 }
 
 const d2 = 23;
@@ -11,10 +11,10 @@ const m2 = 12;
 const a2 = 2021;
 
 function exibirData2() {
-    console.log(`${d2}-${m2}-${a2}`);
+  console.log(`${d2}-${m2}-${a2}`);
 }
 
-// A função só funciona se escrita com o bloco de cófigo {}.
+// A função só funciona se escrita com o bloco de código {}.
 
 exibirData1();
 exibirData2();
