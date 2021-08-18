137a068a8d862bde58d590b4e6f323697e68a800
Yago <yagocarvalho.dev@gmail.com>
2021-08-18 16:41:39 -0300

    Resolução exercício 5

---
+++ m6_exercicios/exercicio5.js
@@ -0,0 +1,11 @@
+/*
+    Crie uma função que receba dois números e retorne se o primeiro é maior ou igual ao segundo.
+*/
+
+function maiorOuIgual(n1, n2) {
+  return n1 >= n2 ? true : false;
+}
+
+console.log(maiorOuIgual(10, 2)); // true
+console.log(maiorOuIgual(10, 10)); // true
+console.log(maiorOuIgual(10, 15)); // false
